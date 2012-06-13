1c102a4764800c25bbb94c63071c2cb21b936264
prophet <prophet@e1357c03-9c8d-804b-bc24-f4024500f409>
2012-06-13 15:00:11 +0000

    lots o work done...
    git-svn-id: svn://projects.prophetsway.net/trunk/ProphetsWay.MyBatisTools@39 e1357c03-9c8d-804b-bc24-f4024500f409

---
+++ BaseDao.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+using IBatisNet.DataMapper;
+
+namespace ProphetsWay.MyBatisTools
+{
+	public abstract class BaseDao
+	{
+		protected ISqlMapper _mapper { get; set; }
+
+		protected BaseDao(Assembly assembly) : this(assembly.GenerateMapper()) { }
+
+		protected BaseDao(ISqlMapper mapper)
+		{
+			_mapper = mapper;
+		}
+
+		protected BaseDao()
+		{
+			_mapper = Assembly.GetAssembly(GetType()).GenerateMapper();
+		}
+
+
+	}
+}
---
+++ MapperFactory.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Configuration;
+using System.IO;
+using System.Reflection;
+using IBatisNet.DataMapper;
+using IBatisNet.DataMapper.Configuration;
+using ProphetsWay.Utilities;
+
+namespace ProphetsWay.MyBatisTools
+{
+	public static class MapperFactory
+	{
+		public static ISqlMapper GenerateMapper(this Assembly callingAssembly)
+		{
+			var assemblyName = callingAssembly.ManifestModule.Name;
+
+			Logger.Debug(string.Format("Generating an ISqlMapper for {0}", assemblyName));
+
+			var builderProps = new NameValueCollection();
+
+			var assemblyUri = new Uri(callingAssembly.CodeBase);
+			var assemblyFile = new FileInfo(assemblyUri.LocalPath);
+			var assemblyPath = assemblyFile.Directory.FullName;
+			var assemblyParts = assemblyName.Split('.');
+			var assemblyLength = assemblyParts.Length;
+
+
+			if (!string.IsNullOrEmpty(ConfigurationManager.AppSettings["MyBatisDBUserName"]))
+				builderProps.Add("username", ConfigurationManager.AppSettings["MyBatisDBUserName"]);
+
+			if (!string.IsNullOrEmpty(ConfigurationManager.AppSettings["MyBatisDBPassword"]))
+				builderProps.Add("password", ConfigurationManager.AppSettings["MyBatisDBPassword"]);
+
+			if (ConfigurationManager.ConnectionStrings["MyBatisDBConnection"] != null)
+				builderProps.Add("connectionString", ConfigurationManager.ConnectionStrings["MyBatisDBConnection"].ConnectionString);
+			
+
+			var builder = new DomSqlMapBuilder { ValidateSqlMapConfig = true, Properties = builderProps };
+			
+			var mapper = builder.Configure(
+				string.Format("{0}{1}SqlMap.{2}.config",
+				              assemblyPath,
+				              Path.DirectorySeparatorChar,
+				              assemblyParts[assemblyLength - 2]));
+			
+			return mapper;
+		}
+	}
+}
