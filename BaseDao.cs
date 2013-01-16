0283487bfbd2c3674358bdf802f19db2d0325ccf
prophet <prophet@e1357c03-9c8d-804b-bc24-f4024500f409>
2013-01-16 03:54:31 +0000

    CRAZY amount of progress on getting the database myBatis stuff working... HUGE!
    git-svn-id: svn://projects.prophetsway.net/trunk/ProphetsWay.MyBatisTools@46 e1357c03-9c8d-804b-bc24-f4024500f409

--- BaseDao.cs
+++ BaseDao.cs
@@ -14,11 +14,14 @@ namespace ProphetsWay.MyBatisTools
 			_mapper = mapper;
 		}
 
+
+		/*		
+		 * commented out until there is a vaild use case
 		protected BaseDao()
 		{
 			_mapper = Assembly.GetAssembly(GetType()).GenerateMapper();
 		}
-
+		//*/
 
 	}
 }
