b9d5c910bff8cb36707d8577f49d5cd08060d06e
prophet <prophet@e1357c03-9c8d-804b-bc24-f4024500f409>
2013-01-19 15:47:40 +0000

    alot more work
    git-svn-id: svn://projects.prophetsway.net/trunk/ProphetsWay.MyBatisTools@47 e1357c03-9c8d-804b-bc24-f4024500f409

---
+++ EnumTypeHandler.cs
@@ -0,0 +1,29 @@
+﻿using IBatisNet.DataMapper.TypeHandlers;
+using ProphetsWay.Utilities;
+
+namespace ProphetsWay.MyBatisTools
+{
+	public abstract	class EnumTypeHandler<T> :ITypeHandlerCallback
+	{
+		public void SetParameter(IParameterSetter setter, object parameter)
+		{
+			setter.Value = ((T) parameter).ToString();
+		}
+
+		public object GetResult(IResultGetter getter)
+		{
+			var output = getter.Value.ToString().GetValue<T>();
+			return output;
+		}
+
+		public object ValueOf(string s)
+		{
+			return s;
+		}
+
+		public object NullValue
+		{
+			get { return default(T); }
+		}
+	}
+}
