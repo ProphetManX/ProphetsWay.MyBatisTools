07e2f04618ef076eb4f23ae9f34d63d367a0b01a
prophet <prophet@e1357c03-9c8d-804b-bc24-f4024500f409>
2014-02-06 14:58:38 +0000

    updated the interface for ibatis calls
    git-svn-id: svn://projects.prophetsway.net/trunk/ProphetsWay.MyBatisTools@66 e1357c03-9c8d-804b-bc24-f4024500f409

---
+++ IBaseDataAccess.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProphetsWay.MyBatisTools
+{
+	public interface IBaseDataAccess
+	{
+		T Get<T>(long id) where T : BaseItem, new();
+	}
+}
